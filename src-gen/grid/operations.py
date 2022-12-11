af72e111dee770b258c7fb75848518325fe1b14c
stanvdkroon <stan.vanderkroon@student.uva.nl>
2022-12-11 13:54:11 +0100

    fixed operations.py

--- src-gen/grid/operations.py
+++ src-gen/grid/operations.py
@@ -1,6 +1,7 @@
 import math
 
 class Callback:
+    path = {}
     def __init__(self, parent):
         self.parent = parent
         pass
@@ -32,25 +33,22 @@ class Callback:
 
     def search(self, cur, target, visited):
         visited.append(cur)
+        if cur == target:
+            return visited
 
-        cell = self.parent.maze.grid[cur[0]][cur[1]]
+        cell = self.parent.maze.grid[cur[1]][cur[0]]
         northWall = cell.walls[0]
         eastWall  = cell.walls[1]
         southWall = cell.walls[2]
         westWall  = cell.walls[3]
 
-        # print('[', cur[0], cur[1], '] [', northWall, eastWall, southWall, westWall,']')
-
         possible_paths = []
         for w, f in [(northWall, self.getNorth), (eastWall, self.getEast), (southWall, self.getSouth), (westWall, self.getWest)]:
             nxt = f(cur)
             if (w == 0 and nxt not in visited and ((0 <= nxt[0] <= 3) and (0 <= nxt[1] <= 3))):
-                if nxt == target:
-                    return visited + [nxt]
-                possible_paths.append(self.search(nxt, target, visited))
+                possible_paths.append(self.search(nxt, target, visited.copy()))
 
         possible_paths = list (filter(lambda x: x != None, possible_paths))
-        # print(possible_paths)
 
         if len(possible_paths) > 0:
             return sorted(possible_paths, key=lambda x: len(x))[0]
@@ -60,9 +58,33 @@ class Callback:
         curX = self.parent.sm.grid.column
         curY = self.parent.sm.grid.row
 
-        # front = cell.walls[parent.sm.grid.orientation]
-        # right = cell.walls[(parent.sm.grid.orientation + 1) % 4]
-        # left  = cell.walls[(parent.sm.grid.orientation - 1) % 4]
-        # back  = cell.walls[(parent.sm.grid.orientation - 2) % 4]
+        path = self.search([curX, curY], [targetX, targetY], [])
+        if path == None:
+            self.path = None
+            self.parent.sm.solve.calculated = True
+            return
 
-        return self.search([curX, curY], [targetX, targetY], [])
+        path_length = len(path)
+        path.append([-1, -1])
+        for i in range(0,path_length):
+            self.path[(path[i][0], path[i][1])] = path[i+1]
+
+        self.parent.sm.solve.calculated = True
+        return
+
+    def get_next(self):
+        if self.path == None:
+            self.parent.sm.solve.target_row = -1
+            self.parent.sm.solve.target_col = -1
+
+        curX = self.parent.sm.grid.column
+        curY = self.parent.sm.grid.row
+        try:
+            next_pos = self.path[(curX, curY)]
+            self.parent.sm.solve.target_row = next_pos[1]
+            self.parent.sm.solve.target_col = next_pos[0]
+        except:
+            self.parent.sm.solve.recalculate = True
+            return
+
+        return
